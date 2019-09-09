3abd7f118387928358654c95b8c88d6db692e1ec
hugh zhao <hughzhao0806@163.com>
2019-09-09 18:28:14 +0800

    查找并显示天气信息

--- app/src/main/java/com/coolweather/android/util/Utility.java
+++ app/src/main/java/com/coolweather/android/util/Utility.java
@@ -5,7 +5,7 @@ import android.text.TextUtils;
 import com.coolweather.android.db.City;
 import com.coolweather.android.db.County;
 import com.coolweather.android.db.Province;
-//import com.coolweather.android.gson.Weather;
+import com.coolweather.android.gson.Weather;
 import com.google.gson.Gson;
 
 import org.json.JSONArray;
@@ -82,9 +82,9 @@ public class Utility {
         return false;
     }
 
-/*    *//**
-     * 将返回的JSON数据解析成Weather实体类
-     *//*
+    /**
+     * 将返回的JSON数据解析成Weather实体类*/
+
     public static Weather handleWeatherResponse(String response) {
         try {
             JSONObject jsonObject = new JSONObject(response);
@@ -95,6 +95,6 @@ public class Utility {
             e.printStackTrace();
         }
         return null;
-    }*/
+    }
 
 }
