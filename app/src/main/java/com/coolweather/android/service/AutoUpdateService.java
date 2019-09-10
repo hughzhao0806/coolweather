e671c03b7912a26cd6fa0cea58296a0a9ebb1c2c
hugh zhao <hughzhao0806@163.com>
2019-09-10 19:58:05 +0800

    增加后台服务，定期刷新天气和背景

---
+++ app/src/main/java/com/coolweather/android/service/AutoUpdateService.java
@@ -0,0 +1,99 @@
+package com.coolweather.android.service;
+
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.IBinder;
+import android.os.SystemClock;
+import android.preference.PreferenceManager;
+
+import com.coolweather.android.gson.Weather;
+import com.coolweather.android.util.HttpUtil;
+import com.coolweather.android.util.Utility;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Response;
+
+public class AutoUpdateService extends Service {
+    public AutoUpdateService() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        updateWeather();
+        updateWeather();
+        AlarmManager manager = (AlarmManager) getSystemService(ALARM_SERVICE);
+        int hour = 8*60*60*1000;//8小时的毫秒数
+        long triggerAtTime = SystemClock.elapsedRealtime()+hour;
+        Intent i = new Intent(this,AutoUpdateService.class);
+        PendingIntent pi = PendingIntent.getService(this,0,i,0);
+        manager.cancel(pi);
+        manager.set(AlarmManager.ELAPSED_REALTIME_WAKEUP,triggerAtTime,pi);
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    /**
+     * 更新天气信息
+     */
+    public void updateWeather(){
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        String weatherString = prefs.getString("weather",null);
+        if(weatherString!=null){
+            //有缓存时直接解析天气数据
+            Weather weather = Utility.handleWeatherResponse(weatherString);
+            String weatherId = weather.basic.weatherId;
+
+
+            String weatherUrl = "http://guolin.tech/api/weather?cityid="+weatherId+"&key=6f7010a73db84d7e9c074fb8dab3f8af";
+            HttpUtil.sendOkHttpRequest(weatherUrl, new Callback() {
+                @Override
+                public void onFailure(Call call, IOException e) {
+                    e.printStackTrace();
+                }
+
+                @Override
+                public void onResponse(Call call, Response response) throws IOException {
+                    String responseText = response.body().string();
+                    Weather weather = Utility.handleWeatherResponse(responseText);
+                    if(weather!=null&&"ok".equals(weather.status)){
+                        SharedPreferences.Editor editor = PreferenceManager.getDefaultSharedPreferences(AutoUpdateService.this).edit();
+                        editor.putString("weather",responseText);
+                        editor.apply();
+                    }
+                }
+            });
+        }
+
+    }
+
+    /**
+     * 更新必应每日一图
+     */
+    public void updateBingPic(){
+        String url = "http://guolin.tech/api/bing_pic";
+        HttpUtil.sendOkHttpRequest(url, new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                String bingPic = response.body().string();
+                SharedPreferences.Editor editor = PreferenceManager.getDefaultSharedPreferences(AutoUpdateService.this).edit();
+                editor.putString("bing_pic",bingPic);
+                editor.apply();
+            }
+        });
+    }
+}
